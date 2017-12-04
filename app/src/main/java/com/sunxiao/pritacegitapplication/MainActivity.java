12355922cb580e85738c2a57fca452bc9406cbbd
sunnnyAuto <18247602177@163.com>
2017-12-04 11:34:02 +0800

    分支第十一次提交

--- app/src/main/java/com/sunxiao/pritacegitapplication/MainActivity.java
+++ app/src/main/java/com/sunxiao/pritacegitapplication/MainActivity.java
@@ -16,12 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         Activity01Older();
-        Activity01Older();
-        Activity01Older();
-        Activity01Older();
-        Activity01Older();
-
-
     }
 
     private void initView() {
