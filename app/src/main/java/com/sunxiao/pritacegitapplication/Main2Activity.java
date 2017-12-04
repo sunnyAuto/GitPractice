27c99b444be846ff01bcdbbc785663bff0acb84f
sunnnyAuto <18247602177@163.com>
2017-12-04 10:12:11 +0800

    第一次提交

---
+++ app/src/main/java/com/sunxiao/pritacegitapplication/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.sunxiao.pritacegitapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    private void Activity02Older(){
+        Log.e("print","activity02"+":::old");
+    }
+}
